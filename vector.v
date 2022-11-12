9d9f65c24af4e7d4e143b3508c7b5adb8b237ee2
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2022-11-12 20:00:09 +0530

    Create vector.v
    vector in verilog

---
+++ vector.v
@@ -0,0 +1,10 @@
+module vector();
+  reg [7:0] a; //vector of reg of 8 bit 
+  reg [2:0] b; // vector of reg 3 bit
+  initial begin
+    a=8'b1100_1010;
+    b=a[6:4]; // vector part select from 6 to 4
+    $display ("a=%b\tb=%b",a,b);
+  end 
+endmodule
+
