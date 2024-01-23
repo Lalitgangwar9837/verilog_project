e57dff253a79e77141a05defde7bcf145190d236
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2024-01-23 21:04:38 +0530

    Update design.v

--- mux_with_parameter_5/design.v
+++ mux_with_parameter_5/design.v
@@ -1,1 +1,15 @@
 
+module mux #(parameter WIDTH =5)
+  (input wire sel, 
+   input wire [WIDTH-1:0] in0,
+   input wire [WIDTH-1:0] in1,
+   output reg [WIDTH-1:0] out );
+  
+  always @*
+    begin
+      if(sel)
+        out=in1;
+      else
+        out=in0;
+    end
+endmodule
